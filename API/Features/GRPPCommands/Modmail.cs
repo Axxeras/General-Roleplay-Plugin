3912de1933a01bb2da3d8925e028506076aea0e0
z5tm <onthebehalfofz5tm@gmail.com>
2026-06-23 18:00:27 -0500

    refactor(modmail): use modern C# features

--- API/Features/GRPPCommands/Modmail.cs
+++ API/Features/GRPPCommands/Modmail.cs
@@ -1,50 +1,54 @@
-﻿using CommandSystem;
-using Exiled.API.Features;
-using GRPP;
-using GRPP.API.Core.Webhooks;
+﻿namespace GRPP.API.Features.GRPPCommands;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Attributes;
+using CommandSystem;
+using EasyTmp;
+using GRPP;
+using Core.Webhooks;
 
-public class ModmailHandler
+public static class ModmailHandler
 {
-    public static int index;
-    public static Dictionary<int, Player> modmailInstances = new Dictionary<int, Player>();
-
+    [OnPluginEnabled]
     public static void InitEvents() => ServerHandlers.WaitingForPlayers += WaitingForPlayers;
-
-    private static void WaitingForPlayers() => index = 0;
+    
+    public static int Index { get; set; }
+    public static Dictionary<int, ExPlayer> ModmailInstances => [];
+    private static void WaitingForPlayers() => Index = 0;
 }
 
 [CommandHandler(typeof(ClientCommandHandler))]
 public class Modmail : ICommand
 {
     public string Command { get; } = "modmail";
-    public string[] Aliases { get; } = new string[] { "mm" };
+    public string[] Aliases { get; } = ["mm"];
     public string Description { get; } = "Sends a message to all moderators currently in the server.";
+    private static readonly string Usage = EasyArgs.Build().CmdArguments(".mm message").Done();
     public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
     {
-        var player = Player.Get(sender);
-
+        var player = ExPlayer.Get(sender);
+        
         if (arguments.Count == 0)
         {
-            response = "Usage: .mm [message]";
+            response = Usage;
             return false;
         }
-
-        ModmailHandler.index += 1;
-        var currentIndex = ModmailHandler.index;
-        ModmailHandler.modmailInstances.Add(currentIndex, player);
-
+        
+        ModmailHandler.Index += 1;
+        var currentIndex = ModmailHandler.Index;
+        ModmailHandler.ModmailInstances.Add(currentIndex, player);
+        
         var message = string.Join(" ", arguments.ToArray());
-
-        foreach (Player target in Player.List)
+        
+        foreach (var target in ExPlayer.List)
         {
-            if (target.RemoteAdminAccess)
-            {
-                target.SendConsoleMessage($"[ModMail] {player.DisplayNickname} ({player.UserId}) (Modmail Index: {currentIndex}): {message}", "yellow");
-                target.Broadcast(10, $"New modmail from {player.DisplayNickname} ({player.UserId}). Check the client console!");
-            }
+            if (!target.RemoteAdminAccess)
+                continue;
+            
+            target.SendConsoleMessage($"[ModMail] {player.DisplayNickname} ({player.UserId}) (Modmail Index: {currentIndex}): {message}", "yellow");
+            target.Broadcast(10, $"New modmail from {player.DisplayNickname} ({player.UserId}). Check the client console!");
         }
         if (!Plugin.Singleton.Config.ModmailCommandWebhookUrl.IsEmpty())
             _ = AsyncWebhookHandler.LogMessage(
@@ -61,34 +65,36 @@ public class Modmail : ICommand
 [CommandHandler(typeof(RemoteAdminCommandHandler))]
 public class Reply : ICommand
 {
-    public string Command { get; } = "reply";
-    public string[] Aliases { get; } = new string[] { "r" };
+    public string Command { get; } = "modMailReply";
+    public string[] Aliases { get; } = ["mmr", "modmail"];
     public string Description { get; } = "Replies to a modmail message using the index provided in the message.";
+    private static readonly string Usage = EasyArgs.Build().CmdArguments("mmr index message").Done();
+    
     public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
     {
-        var player = Player.Get(sender);
+        var player = ExPlayer.Get(sender);
         if (arguments.Count < 2)
         {
-            response = "Usage: .reply [index] [message]";
+            response = Usage;
             return false;
         }
-        if (!int.TryParse(arguments.At(0), out int index))
+        if (!int.TryParse(arguments.At(0), out var index))
         {
             response = "The index must be a valid integer.";
             return false;
         }
-        if (!ModmailHandler.modmailInstances.ContainsKey(index))
+        if (!ModmailHandler.ModmailInstances.ContainsKey(index))
         {
             response = "No modmail message found with the provided index.";
             return false;
         }
-
-        var target = ModmailHandler.modmailInstances[index];
+        
+        var target = ModmailHandler.ModmailInstances[index];
         var message = string.Join(" ", arguments.Skip(1).ToArray());
-
+        
         target.SendConsoleMessage($"[ModMail Reply] {player.DisplayNickname}[Staff]: {message}", "yellow");
         target.Broadcast(10, "You have received a reply to your modmail message. Check the client console!");
-
+        
         response = $"Reply sent to {target.DisplayNickname}!";
         return true;
     }
